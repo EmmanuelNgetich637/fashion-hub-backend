c51f73824b9cbdc7b1d632e6e644b4f06de969c0
Emmanuel Ngetich <engetich333@gmail.com>
2025-06-24 01:49:16 -0700

    Add Product and Order models with migrations

---
+++ app/models/order.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column9db.Float, nullable=False
+
+    user = db.relationship('User', backref='orders')
+    product = db.relationship('Product', backref='orders')
+    
---
+++ app/schemas/product_schema.py
@@ -0,0 +1,16 @@
+from app.extensions import ma
+from app.models.product import Product
+
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+        id = ma.auto_field()
+        name = ma.auto_field()
+        price = ma.auto_field()
+        description = ma.auto_field()
+        stock = ma.auto_field()
+
+        product_schema = ProductSchema()
+        products_schema = ProductsSchema(many=True)
+        
